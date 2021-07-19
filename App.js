97a461cf7f9aa75b7104d77240fcd4870195e476
Namkx <namlee0005@gmail.com>
2021-07-19 22:56:07 +0700

    push appstore

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import 'react-native-gesture-handler';
 import { StoreProvider } from '@src/store';
 import Routes from '@src/routes';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import SplashScreen from 'react-native-splash-screen';
 import { AdManager } from 'react-native-admob-native-ads';
 
 AdManager.setRequestConfiguration({
@@ -15,6 +15,10 @@ AdManager.setRequestConfiguration({
 const App = () => {
   // eslint-disable-next-line no-console
   console.disableYellowBox = true;
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
 
   return (
     <StoreProvider>
